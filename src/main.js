2e0836588fa5fd204973f62903b5ede7e681e788
FzeroH <vampir666x@gmail.com>
2022-06-03 23:19:35 +0300

    feat(main.js): use vue-router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router';
 
-createApp(App).mount('#app');
+require('dotenv').config();
+
+createApp(App)
+  .use(router)
+  .mount('#app');
