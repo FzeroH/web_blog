1df9c0fc35b582038e5e2201c5cb27819983bb65
FzeroH <vampir666x@gmail.com>
2022-06-03 23:27:08 +0300

    feat(Article.vue): create article template

---
+++ src/components/Article.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h3>{{ name }}</h3>
+    <h5>{{ author }}</h5>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Article',
+  props: {
+    name: String,
+    author: String,
+    text: String,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+
+div{
+  display: flex;
+  flex-direction: column;
+}
+
+h3 {
+  font-size: 24px;
+  margin: 10px 0 5px 0;
+}
+
+h5 {
+  font-size: 16px;
+  margin-top: 5px;
+}
+</style>
