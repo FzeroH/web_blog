12746e8465d5c928b99ec31a32c54dc93cdee49a
FzeroH <vampir666x@gmail.com>
2022-06-12 23:47:27 +0300

    feat(ListArticlesPage.vue): add getArticlesList and add data in props

--- src/components/ListArticlesPage.vue
+++ src/components/ListArticlesPage.vue
@@ -1,53 +1,22 @@
 <template>
   <h2>The blog</h2>
-  <LastArticle :name="'Как не сдохнуть'"
-               :author="'Авторный автор'"
-               :text="'Короче, старайся не умереть и всё'"
+  <LastArticle :name="`${ lastArticle.blog_title }`"
+               :author="`${ lastArticle.user_name }`"
+               :text="`${ lastArticle.blog_text }`"
   />
   <div class="article-container">
-    <Article
-        :name="'Как не сдохнуть'"
-        :author="'Авторный автор'"
-        :text="'Короче, старайся не умереть и всё'"/>
-    <Article
-        :name="'Как не сдохнуть'"
-        :author="'Авторный автор'"
-        :text="'Короче, старайся не умереть и всё'"/>
-    <Article
-        :name="'Как не сдохнуть'"
-        :author="'Авторный автор'"
-        :text="'Короче, старайся не умереть и всё'"/>
-    <Article
-        :name="'Как не сдохнуть'"
-        :author="'Авторный автор'"
-        :text="'Короче, старайся не умереть и всё'"/>
-    <Article
-        :name="'Как не сдохнуть'"
-        :author="'Авторный автор'"
-        :text="'Короче, старайся не умереть и всё'"/>
-    <Article
-        :name="'Как не сдохнуть'"
-        :author="'Авторный автор'"
-        :text="'Короче, старайся не умереть и всё'"/>
-    <Article
-        :name="'Как не сдохнуть'"
-        :author="'Авторный автор'"
-        :text="'Короче, старайся не умереть и всё'"/>
-    <Article
-        :name="'Как не сдохнуть'"
-        :author="'Авторный автор'"
-        :text="'Короче, старайся не умереть и всё'"/>
-    <Article
-        :name="'Как не сдохнуть'"
-        :author="'Авторный автор'"
-        :text="'Короче, старайся не умереть и всё'"/>
+    <Article v-for="(article, index) in articlesList" :key="index"
+        :name="article.blog_title"
+        :author="article.user_name"
+        :text="article.blog_text"/>
 
   </div>
 </template>
 
 <script>
 
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
+import BlogService from '@/api/BlogService';
 import Article from './Article.vue';
 import LastArticle from './LastArticle.vue';
 
@@ -60,7 +29,23 @@ export default defineComponent({
   },
   setup() {
     const userData = ref();
-    return { userData };
+    const articlesList = ref([]);
+    const lastArticle = ref([]);
+    const getArticlesList = async () => {
+      await BlogService.getArticlesList()
+        .then((res) => {
+          articlesList.value = res;
+          articlesList.value = articlesList.value.reverse();
+          lastArticle.value = articlesList.value.shift();
+        })
+        .catch((error) => console.error(error.message));
+    };
+    onMounted(() => {
+      getArticlesList();
+      console.log(articlesList);
+      console.log(lastArticle);
+    });
+    return { userData, articlesList, lastArticle };
   },
 });
 </script>
